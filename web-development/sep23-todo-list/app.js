8daa306e62bbca540d03bb7e96a316151707488a
hoangdesu <hoangdesu@gmail.com>
2024-09-30 20:48:25 +0700

    todo list: style the app, remove a todo, persists in localStorage

--- web-development/sep23-todo-list/app.js
+++ web-development/sep23-todo-list/app.js
@@ -2,7 +2,58 @@ const form = document.querySelector('form');
 const todosContainer = document.querySelector('#todos-container')
 
 // array to keep track of all the TODOs
-const todoList = [];
+// let todoList = ['do hw', 'learn html', 'finish javascript'];
+
+const localStorageTodoList = localStorage.getItem('todoList');
+let todoList = []
+
+if (localStorageTodoList) {
+    todoList = JSON.parse(localStorageTodoList);
+}
+
+// arrow function
+const renderTodoList = () => {
+    todoList.forEach(task => {
+        // only create an li element IN THE MEMORY
+        const li = document.createElement('li');
+
+        // asign the string to the li
+        li.textContent = task;
+        li.className = 'todo-item';
+
+        // create a delete button
+        const delBtn = document.createElement('button');
+        delBtn.textContent = '🗑️';
+
+        // add click event listener to the del button
+        delBtn.addEventListener('click', () => {
+            console.log('deleting...', task);
+
+            // removing task
+            todoList = todoList.filter(todo => todo !== task);
+            
+            console.log('todolist:', todoList);
+
+            // update the new todoList inside localStorage
+            localStorage.setItem('todoList', JSON.stringify(todoList));
+
+            // reset the UI
+            todosContainer.innerHTML = ''
+
+            // render the new todo list
+            renderTodoList(todoList);
+        });
+
+        // attach button to li element
+        li.append(delBtn);
+
+        // add the newly created li element into container
+        todosContainer.append(li);
+    });
+}
+
+// show all the todos when the app loads for the first time
+renderTodoList();
 
 form.addEventListener('submit', (e) => {
     // stop the page from reloading
@@ -13,6 +64,10 @@ form.addEventListener('submit', (e) => {
 
     // add new todo to list, to render on the screen later
     todoList.push(todoInput.value);
+
+    // turn the whole todo array into a string
+    // and save it to local storage for later use
+    localStorage.setItem('todoList', JSON.stringify(todoList));
     
     // render all the TODOs into the container
     // first wrap every todo inside an <li>todo</li>
@@ -25,15 +80,5 @@ form.addEventListener('submit', (e) => {
     // reset the list before rendering a new one
     todosContainer.innerHTML = '';
     
-    todoList.forEach(task => {
-        // only create an li element IN THE MEMORY
-        const li = document.createElement('li');
-
-        // asign the string to the li
-        li.textContent = task;
-
-        // add the newly created li element into container
-        todosContainer.append(li);
-    });
-
+    renderTodoList(todoList);
 });
